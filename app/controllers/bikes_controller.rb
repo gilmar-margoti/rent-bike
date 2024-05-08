d6a269b3cf5f317fe03a912a2234087bfa9c4d2d
max cody <Max.cody1@outlook.com>
2024-05-08 13:17:24 +0100

    draft of the creating bikes

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -9,9 +9,17 @@ class BikesController < ApplicationController
     @bike = Bike.find(params[:id])
   end
   def create
+    puts params.inspect
     @bike = Bike.new(bike_params)
+    if @bike.save
+      raise
+      redirect_to bike_path(@bikes)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+  private
   def bike_params
-    params.require(:bike).permit(:id, :location, :price_per_day, :condition, :category)
+    params.require(:bike).permit(:location, :price_per_day, :condition, :category)
   end
 end
--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,7 +1,7 @@
 class Bike < ApplicationRecord
   belongs_to :user
 
-  validates :location, :price_per_day, presence: true
-  validates :condition, numericality: { only_integer: true }
-  validates :condition, inclusion: { in: 0..5 }
+  # validates :location, :price_per_day, presence: true
+  # validates :condition, numericality: { only_integer: true }
+  # validates :condition, inclusion: { in: 0..5 }
 end
