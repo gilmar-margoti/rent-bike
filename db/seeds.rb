39a8d5e81ef52ec100a28da7fcdd72944710fc3a
max cody <Max.cody1@outlook.com>
2024-05-10 12:31:29 +0100

    new seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ puts "Users created successfully..."
 puts "Creating bikes..."
 Bike.create!(location: "Putney",           price_per_day: 5.00,  category: "Road bike",    condition: 3, user: gilmar)
 Bike.create!(location: "Shoreditch",       price_per_day: 25.00, category: "Eletric bike", condition: 5, user: ella)
-Bike.create!(location: "Richmond",         price_per_day: 15.00, category: "Road bike",    condition: 4, user: max)
+Bike.create!(location: "Richmond london",         price_per_day: 15.00, category: "Road bike",    condition: 4, user: max)
 Bike.create!(location: "Fulham",           price_per_day: 8.00,  category: "Hybrid bike",  condition: 3, user: gilmar)
 Bike.create!(location: "Soho",             price_per_day: 10.00, category: "BMX",          condition: 3, user: ella)
 Bike.create!(location: "Acton",            price_per_day: 13.00, category: "Road bike",    condition: 3, user: max)
@@ -22,4 +22,21 @@ Bike.create!(location: "Battersea",        price_per_day: 6.00,  category: "Hybr
 Bike.create!(location: "Clapham Junction", price_per_day: 8.00,  category: "Road bike",    condition: 2, user: ella)
 puts "Bicycles created successfully..."
 
+locations = [
+  "London", "wembley", "whitechapel", "temple london", "southwark", "bermondsey",
+  "rotherhithe", "belgravia", "notting hill", "silvertown", "poplar", "Brixton",
+  "blackwall", "millwall", "bexleyheath", "bayswater", "westbourne green", "somers town", "clerkenwell",
+  "brompton", "kensington", "lewisham", "deptford", "westiminster", "lambeth", "surrey quays",
+  "finsbury park", "farringdon", "limehouse"
+]
+puts "Creating bikes..."
+40.times do
+  location = locations.sample
+  price_per_day = rand(5..30).round(2)
+  category = ["Road bike", "Electric bike", "Hybrid bike", "BMX"].sample
+  condition = rand(1..5)
+  user = [gilmar, ella, max].sample  # Assuming gilmar, ella, max are predefined users
+  Bike.create!(location: location, price_per_day: price_per_day, category: category, condition: condition, user: user)
+end
+puts "Bicycles created successfully..."
 puts "Finished"
